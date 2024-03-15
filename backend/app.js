64d8b76d5f7b4222e8831ab34f553646cc93e1b3
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-03-15 14:14:50 -0400

    Basic working website
    Updated app and layouts to create basic website functionality to build from.

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,17 @@
 import express from 'express';
 let app = express();
 
-
 // constants
 const HOST = '127.0.0.1';
 const PORT = 3000;
 
 
-
 // initialize EJS template engine
 app.set('view engine', 'ejs');
+// Set the views directory
+app.set('views', './backend/views');
+// set static asset folder for EJS
+app.use(express.static('public'));
 
 // __dirname workaround for ES module
 import path from 'path';
@@ -23,21 +25,19 @@ const __dirname = path.dirname(__filename);
 
 // default route
 app.get('/', (req, res) => {
-    res.render('home', { pageTitle: 'Guardian Link' });
+    res.render('pages/home', { pageTitle: 'Home' });
 });
 
 
 
-// set static asset folder for EJS
-app.use(express.static('public'));
-
 // start server
 app.listen(PORT, HOST, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server running at http://${HOST}:${PORT}/`)
 } );
 
 // kill server
 process.on('SIGINT', () => {
     process.exit(0)
     console.log('Server closed')
-});
+});// set static asset folder for EJS
+app.use(express.static('public'));
