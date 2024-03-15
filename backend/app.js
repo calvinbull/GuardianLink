1fafb3bd128db72f3eef11def1e674dff09186dc
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-03-15 11:33:28 -0400

    Update app.js
    implemented basic app logic including the use of EJS templates

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,37 @@
 // entry point for server
+
+import express from 'express';
+let app = express();
+
+// initialize EJS template engine
+app.set('view engine', 'ejs');
+
+// __dirname workaround for ES module
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// route definitions
+
+// default route
+app.get('/', (req, res) => {
+    res.render('home', { pageTitle: 'Guardian Link' });
+});
+
+
+
+// set static asset folder for EJS
+app.use(express.static('public'));
+
+// start server
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+} );
+
+// kill server
+process.on('SIGINT', () => {
+    process.exit(0)
+    console.log('Server closed')
+});
