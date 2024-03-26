74eee7fb32b7146a60919731968b070446598382
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-03-26 09:10:15 -0400

    Activated logout button
    Should now send post request correctly to logout.

--- public/js/header.js
+++ public/js/header.js
@@ -2,3 +2,18 @@
 // alter header to display 'home/login/register' when logged out
 // display 'home/connections/messages/my account/logout' when logged in
 
+// enable logout button to send post request
+document.getElementById('logout').addEventListener('click', function(event) {
+    // prevent the default action
+    event.preventDefault(); 
+
+    // sent logout post request
+    fetch('/logout', {
+        method: 'POST',
+    }).then(function() {
+        // redirect to home
+        window.location.href = '/';
+    }).catch(function(error) {
+        console.error('Error logging out user: ', error);
+    });
+});
