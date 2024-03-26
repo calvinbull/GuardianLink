3a50b33accadeae6d64c11d60cda01188e12c57f
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-03-26 14:39:54 -0400

    Create propertyLabels.js
    Created dictionary for displaying account properties in a more readable format.

---
+++ public/js/propertyLabels.js
@@ -0,0 +1,18 @@
+// define dictionary to make displayed property labels more 'human-readable'
+
+const propertyLabels = {
+    userID: 'User ID',
+    accountType: 'Account Type',
+    name: 'Name',
+    username: 'Username',
+    email: 'Email',
+    availability: 'Available Hours per Week',
+    backgroundCheck: 'Possesses Valid Background Check?',
+    isCurrentlyAvailable: 'Is Currently Available?',
+    linkedin: 'LinkedIn',
+    concerns: 'Organizational Concerns',
+    missionStatement: 'Mission Statement'
+};
+
+// export
+module.exports = propertyLabels;
