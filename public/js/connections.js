260b80a7ce433577baf060e686cd66cf2efdefe2
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-03-27 10:26:20 -0400

    Implemented search/filter function
    Connections page can now be searched with dynamic filtering. Currently name, username, and email can be used to search/filter. Codebase is set up to easily allow any other property to be used if desired in the future.

--- public/js/connections.js
+++ public/js/connections.js
@@ -1,1 +1,25 @@
-// TODO
+// add functionality to filter displayed accounts
+document.addEventListener("DOMContentLoaded", function() {
+
+    searchInput.addEventListener("input", function() {
+        
+        // get search string, convert to lower for consitency 
+        const query = document.getElementById("searchInput").value.trim().toLowerCase();
+
+        // get selected filtering property and add it to 'account-' to match related class
+        const filter = ".account-" + document.getElementById("filterSelection").value;
+
+        // select all account cards
+        const accountCards = document.querySelectorAll(".account-card");
+
+        accountCards.forEach(function(card) {
+            const name = card.querySelector(filter).textContent.toLowerCase();
+
+            if (name.includes(query)) {
+                card.style.display = "block"; // Show the card if it matches the query
+            } else {
+                card.style.display = "none"; // Hide the card if it doesn't match
+            }
+        });
+    });
+});
