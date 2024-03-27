e396fca141ff7a8633ecab5171b8d3fa81cfec7b
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-03-27 17:03:31 -0400

    Update logger.js
    Updated logger to wipe old files on startup.

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,6 +1,17 @@
 // set up logger
+const fs = require('fs');
 const winston = require('winston');
 const { combine, timestamp, json } = winston.format;
+
+// Delete existing log files
+try {
+    fs.unlinkSync('error.log');
+    fs.unlinkSync('combined.log');
+} catch (err) {
+    // Handle file deletion error if log files don't exist
+    console.error('Error deleting log files:', err);
+}
+
 const logger = winston.createLogger({
     level: 'info',
     format: combine(timestamp(), json()),
