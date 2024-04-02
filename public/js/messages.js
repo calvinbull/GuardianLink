a45ddf2c147bdb43362dda396b03f808acf4ad3e
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-04-02 15:06:07 -0400

    Improved visual layout of messages display

--- public/js/messages.js
+++ public/js/messages.js
@@ -12,6 +12,21 @@ document.addEventListener("DOMContentLoaded", function() {
         dataList.appendChild(option);
     });
 
+    // try to auto-populate default conversation header
+    try {
+        // note the existingConvs format: [{ userID: account.userID, name: account.name, messages: [] }]
+
+        // get user's name in the first list item
+        var firstUser = existingConvs[0].name;
+        console.log(existingConvs);
+        // update the conversation header
+        document.getElementById('conversation-header').innerText = firstUser;
+
+
+    } catch (err) {
+        console.log("Error populating conversation header.");
+    }
+
 });
 
 
@@ -46,4 +61,25 @@ document.getElementById('sendMessage').addEventListener('submit', function(event
         console.log('Error updating user information.');
     });
 
+});
+
+// update conversation header block name & 'to' block when selecting a user
+document.addEventListener("DOMContentLoaded", function() {
+    // fetch list of conversations from dynamic 'list-group'
+    var conversations = document.querySelectorAll('.list-group-item');
+
+    // click event listeners to each conversation selection
+    conversations.forEach(function(conversation) {
+        conversation.addEventListener('click', function(event) {
+            // Prevent default link behavior
+            event.preventDefault();
+
+            // get user's name
+            var userName = conversation.innerText.trim();
+            // update the conversation header
+            document.getElementById('conversation-header').innerText = userName;
+
+
+        });
+    });
 });
