cbf1dd7f996d6533209011ec7934b7780ed41796
calvinbull <97459530+calvinbull@users.noreply.github.com>
2024-04-04 18:25:08 -0400

    Initial population of home scrueen
    About us scrolling needs to be improved.

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+// scroll down when 'learn more' button is pressed
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var learnMoreButton = document.getElementById('learnMore');
+    var aboutDiv = document.getElementById('about');
+
+    // not working as intended
+    learnMoreButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: aboutDiv.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+
+})
